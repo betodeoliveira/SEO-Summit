f9a01799ea484635637adf2cc8b331de98924ada
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-12 15:09:17 -0300

    Liberando acesso sem verificação de cookie na url de teste

--- cookie-checker-global.js
+++ cookie-checker-global.js
@@ -1,9 +1,14 @@
-// First checks if the user already has the cookie or not
-var cookieName = "seo-summit-2022";
-if (typeof Cookies.get(cookieName) !== "undefined") {
-  // console.log("cookie exists");
-  $(".evento-display-trigger").click();
-} else {
-  // console.log("cookie doesn't exists");
-  window.location.href = "/evento";
+if (window.location.href.indexOf("webflow.io") > -1) {
+  // it is on test mode
+}
+else {
+  // First checks if the user already has the cookie or not
+  var cookieName = "seo-summit-2022";
+  if (typeof Cookies.get(cookieName) !== "undefined") {
+    // console.log("cookie exists");
+    $(".evento-display-trigger").click();
+  } else {
+    // console.log("cookie doesn't exists");
+    window.location.href = "/evento";
+  }
 }
