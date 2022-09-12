f0ad8cb48976651410229d4732991fb24e7f95a0
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-12 15:15:49 -0300

    Mostra o conteúdo em urls de testes

--- cookie-checker-global.js
+++ cookie-checker-global.js
@@ -1,5 +1,6 @@
 if (window.location.href.indexOf("webflow.io") > -1) {
   // it is on test mode
+  $(".evento-display-trigger").click();
 }
 else {
   // First checks if the user already has the cookie or not
